f1b05ae9e19895c3fba6847a79a77f2affa37767
victormwachi <mudakizevictor@gmail.com>
2023-10-23 23:35:00 +0300

    crud

--- 06tut/controllers/employeesControllers.js
+++ 06tut/controllers/employeesControllers.js
@@ -1,29 +1,47 @@
-const data ={}
-
-data.employees = require('../data/employees.json');
+const data ={
+	employees: require('../model/employees.json'),
+	setEmployees: function(data){
+		this.employees = data
+	}
+}
 
 const getAllEmployees = (req,res) => {
-	res.json(data.employess);
+	res.json(data.employees);
 }
 
 const updateEmployee = (req,res) => {
-		res.json({
-			"firstname": req.body.firstname,
-			"lastname": req.body.lastname
-		})
+	let arrItemIdx;
+	const employee = data.employees.filter((item,idx,arr) => {
+		item.id==req.params.id ? arrItemIdx=idx: undefined;
+		return item
+	})
+	if(arrItemIdx){
+		employee.firstname = req.body.firstname
+		employee.lastname = req.body.lastname
+		data.employees.splice(arrItemIdx,1,employee)
+		data.setEmployees(data.employees)
+		res.json(data.employees)
 	}
+	else {
+		res.json("error");
+	}
+}
 
 const addEmployee = (req,res) => {
-	res.json({
-		"firstname": req.body.firstname,
-		"lastname": req.body.lastname
-	})
+	const newEmployee = {}
+	newEmployee.id = data.employees.length + 1 || 1
+	newEmployee.firstname = req.body.firstname
+	newEmployee.lastname = req.body.lastname
+	data.employees.push(newEmployee);
+	data.setEmployees(data.employees)
+	res.json(data.employees)
 }
 
 const deleteEmployee = (req,res) => {
 	res.json({"id":req.body.id})
 }
 
-const getEmployee = (req,res) => {res.json({"id":req.params.id})}
+//get employee with corresponding id, assume id is unique for every employee
+const getEmployee = (req,res) => res.json(data.employees.filter(item => item.id==req.params.id)[0])
 
 module.exports = {getAllEmployees, addEmployee,updateEmployee,deleteEmployee,getEmployee}
--- 06tut/server.js
+++ 06tut/server.js
@@ -4,23 +4,8 @@ const path = require("path");
 const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 const {logger} = require('./middleware/logEvents');
-const {errorHandler} = require('./middleware/errorHandler')
-
-
-//whitelist
-const whitelist = ['http://localhost:3500','http://127.0.0.1:3500']//,'https://www.google.com'];
-const corsOptions = {
-    origin:(origin,callback) => {
-        if(whitelist.indexOf(origin)!== -1 || !origin){
-            callback(null,true);
-        }
-        else{
-            callback(new Error("Not allowed by cors"));
-        }
-    },
-    optionsSuccessStatus:200
-}
-
+const {errorHandler} = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOptions')
 //cross origin resource sharing
 app.use(cors(corsOptions));
 
